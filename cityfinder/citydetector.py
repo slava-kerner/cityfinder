f4af4ab037b0cb31f5f01b60df6553f8b0e8b69c
slava <slava@satellogic.com>
2017-11-22 09:44:40 +0200

    print radii

--- cityfinder/citydetector.py
+++ cityfinder/citydetector.py
@@ -8,13 +8,15 @@ class CityDetector:
         self.config = config or self.default_config()
         self.debug_path = debug_path
 
-    def find_circle_city(self, path):
+    def find_circle_city(self, path, out_path=None, imshow=False):
         img = cv2.imread(path, 0)
-        img = cv2.medianBlur(img, 5)
+        # img = cv2.medianBlur(img, 5)
 
         config = self.config['hough_circle']
         circles = cv2.HoughCircles(img, **config)
-        print('Detected %d circles' % len(circles))
+        if circles is None or circles[0] is None or len(circles[0]) == 0:
+            return []
+
         # circles = cv2.HoughCircles(img, config['method'], config['dp'], config['mid_dist'],..)
 
         cimg = cv2.cvtColor(img, cv2.COLOR_GRAY2BGR)
@@ -25,9 +27,14 @@ class CityDetector:
             # draw the center of the circle
             cv2.circle(cimg, (i[0], i[1]), 2, (0, 0, 255), 3)
 
+        if imshow:
             cv2.imshow('detected circles',cimg)
             cv2.waitKey(0)
             cv2.destroyAllWindows()
+
+        if out_path is not None:
+            cv2.imwrite(out_path, cimg)
+        return circles[0]
 
     @classmethod
     def default_config(cls):
@@ -39,7 +46,7 @@ class CityDetector:
                 'param1': 50,
                 'param2': 30,
                 'minRadius': 0,
-                'maxRadius': 0,
+                'maxRadius': 20,
             },
         }
         return config
--- tests/test_citydetector.py
+++ tests/test_citydetector.py
@@ -1,14 +1,37 @@
-import cv2
+import os
 
+import numpy as np
 import unittest
 from cityfinder.citydetector import CityDetector
 
 
 class TestCityDetector(unittest.TestCase):
-    def test_find_circle_city(self):
-        path = 'data/city/tulkarm.png'
-        debug_path = 'out/debug'
+    in_folder = 'data/city'
+    out_folder = 'out'
+    debug_folder = 'out/debug'
+
+    def _path(self, name):
+        return os.path.join(self.in_folder, name + '.png')
+
+    def test_optimize_params(self):
+        name = 'tulkarm'
 
         config = CityDetector.default_config()  # and override
-        detector = CityDetector(config=config, debug_path=debug_path)
-        detector.find_circle_city(path)
+        for config['hough_circle']['dp'] in np.arange(.5, 2.5, .5):
+            for config['hough_circle']['maxRadius'] in range(5, 40, 5):
+                config_text = '_maxRadius=%0.2f_dp=%0.2f' % (config['hough_circle']['maxRadius'], config['hough_circle']['dp'])
+                out_path = os.path.join(self.out_folder, name + config_text + '.png')
+                detector = CityDetector(config=config)
+                circles = detector.find_circle_city(self._path(name), out_path=out_path, imshow=False)
+                print('config: %s. Detected %d circles' % (config_text, len(circles)))
+
+    def test_radii_histogram(self):
+        name = 'tulkarm'
+
+        config = CityDetector.default_config()  # and override
+        config['hough_circle']['dp'] = 2
+        config['hough_circle']['maxRadius'] = 20
+        detector = CityDetector(config=config)
+        circles = detector.find_circle_city(self._path(name))
+        radii = [c[1] for c in circles]
+        print(sorted(radii))
