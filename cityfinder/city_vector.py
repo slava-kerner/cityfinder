d81171c90fbb01b2c8b9c8e1cfa02fff01aa25d7
slava <slava@satellogic.com>
2017-11-23 03:30:10 +0200

    azimuths

--- cityfinder/city_vector.py
+++ cityfinder/city_vector.py
@@ -2,7 +2,7 @@ import numpy as np
 
 
 class CityVector:
-    """ {city -> (lon, lat)}. in future consider radius. """
+    """ {city -> (x, y)}, where x,y are ideally lon,lat. in future consider radius. """
     def __init__(self, other=None):
         if other is not None:
             if isinstance(other, dict):
@@ -13,3 +13,10 @@ class CityVector:
                 raise NotImplementedError
         else:
             self.cities = {}
+
+    def _calc_azimuths(self):
+        def azimuth(xy1, xy2):
+            return np.rad2deg(np.math.atan2(xy2[1] - xy1[1], xy2[0] - xy1[0]))
+
+        azimuths = {(city1, city2): azimuth(self.cities[city1], self.cities[city2])
+                    for city1 in self.cities for city2 in self.cities if city1 != city2}
